39d6e64dbe3ce41d6fbee8c0701f4d2e51b0abbf
Mukund-10 <47919779+Mukund-10@users.noreply.github.com>
2019-02-23 21:22:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import command
 
-Token = "NTQ4NTMyMTQ4MjcwMDA2Mjcy.D1LmHw.vofXTbKXgYFBifEUhEMVYi-dChY"
+Token = "NTQ4ODg3OTUzODU5MDE4Nzgy.D1L3kg.pTuhu2ppXdMfkusmJxLXJEZYRso"
 
 client = commands.Bot(command_prefix ='e!')
 
 @client.event
 async def on_ready();
+     await client.change_presence(game=discord.Game(name='beta test'))
      print('Bot is ready')
 
  client.run(TOKEN)
